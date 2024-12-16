75088007888d1d4a59c89dd1177ff810b5f70c03
bilalrashidweuno <bilalrashid@weuno.co>
2024-12-16 16:28:48 +0200

    vercel error fixes

--- src/store/provider.tsx
+++ src/store/provider.tsx
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux'
 import { makeStore, AppStore } from './store'
 
 export default function StoreProvider({ children, initialState }: { children: React.ReactNode; initialState: any }) {
-    const storeRef = useRef<AppStore>()
+    const storeRef = useRef<AppStore>(null)
     if (!storeRef.current) {
         storeRef.current = makeStore(initialState)
     }
