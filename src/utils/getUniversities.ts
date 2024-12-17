2ca2a51e65f2c2a2b2e4c162d4d05f3255fb38e3
Ms <contactevelab@gmail.com>
2024-12-17 16:16:44 +0000

    university tab fix

--- src/utils/getUniversities.ts
+++ src/utils/getUniversities.ts
@@ -1,24 +1,25 @@
-async function getUniversities() {
-  const apiUrl = "http://universities.hipolabs.com/search"; //list of universities API
-  
-  try {
-    // Fetch data from the API
-    const response = await fetch(apiUrl);
-    const universities = await response.json();
+import axios from 'axios';
 
+const API_URL = 'https://api-quality.seekrsgroup.com/v1/auth/get-universities'; // Replace with your actual API endpoint
+
+const getUniversities = async () => {
+  try {
+    const response = await axios.get(API_URL);
+    // Assuming the response data is an array of universities
+    const universities = response.data;
+
+    //console.log("uni list", universities);
     // Transform data into the desired format
-    const formattedUniversities = (universities as Array<{ name: string, country: string }>).map((uni) => ({
+    const formattedUniversities = (universities as Array<{ name: string}>).map((uni) => ({
       value: uni.name,
       label: uni.name,
-      country: uni.country,
     }));
-
-    return formattedUniversities;
-
+    return formattedUniversities; // Adjust this based on the actual structure of your API response
   } catch (error) {
     console.error("Error fetching universities:", error);
+    throw error; // Rethrow the error to handle it in the calling function if needed
   }
-}
+};
 
 export default getUniversities;
 
