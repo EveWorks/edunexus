a34035ef4d89883d3047ad7af031932610b683a0
bilalrashidweuno <bilalrashid@weuno.co>
2024-12-27 09:53:25 +0200

    deepgram config wait for 1 sec to send message

--- src/components/chats/index.tsx
+++ src/components/chats/index.tsx
@@ -57,7 +57,7 @@ const Chat = ({ id }: { id: string }) => {
         utterance_end_ms: 3000,
         interim_results: true,
         vad_events: true,
-        endpointing: 300,
+        endpointing: 1000,
       });
     }
   }, [microphoneState]);
